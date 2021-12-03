d8c1f7fda9ac5e5ec08a8df33f84c8d7688cfec9
Brandon Tyus <ausis@mail.com>
2021-12-03 14:08:53 -0600

    centers login form and adjusts size

--- frontend/rev-tracker/src/Login.js
+++ frontend/rev-tracker/src/Login.js
@@ -1,27 +1,36 @@
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 function Login() {
     return (
         <>
             <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                <Row>
+                    <Col xs={{span: 3, offset: 4}}>
+                        <Form.Group className="mb-3 center" controlId="formBasicEmail">
+                            <Form.Label className="text-center">Email address</Form.Label>
+                            <Form.Control size="md" type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={{span: 3, offset: 4}}>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control size="md" type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                        Submit
+                        </Button>
+                    </Col>
+                </Row>
             </Form>
         </>
     );
