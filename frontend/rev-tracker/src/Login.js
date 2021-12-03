1081a8bbdeedb56a93743e1d5b9d803e73df6303
Brandon Tyus <ausis@mail.com>
2021-12-03 16:01:18 -0600

    centers login component

--- frontend/rev-tracker/src/Login.js
+++ frontend/rev-tracker/src/Login.js
@@ -2,12 +2,13 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
+import './Main.css'
 function Login() {
     return (
-        <>
-            <Form>
+        <div className="d-flex align-items-center justify-content-center">
+            <Form style={{ position: 'absolute', left: '50%', top: '45%', transform: 'translate(-50%, -50%)' }}>
                 <Row>
-                    <Col xs={{span: 3, offset: 4}}>
+                    <Col>
                         <Form.Group className="mb-3 center" controlId="formBasicEmail">
                             <Form.Label className="text-center">Email address</Form.Label>
                             <Form.Control size="md" type="email" placeholder="Enter email" />
@@ -18,7 +19,7 @@ function Login() {
                     </Col>
                 </Row>
                 <Row>
-                    <Col xs={{span: 3, offset: 4}}>
+                    <Col>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
                             <Form.Control size="md" type="password" placeholder="Password" />
@@ -32,7 +33,7 @@ function Login() {
                     </Col>
                 </Row>
             </Form>
-        </>
+        </div>
     );
 }
 
