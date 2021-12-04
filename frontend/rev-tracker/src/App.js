056657394c2386fef3140ece911401df53d0cd72
Brandon Tyus <ausis@mail.com>
2021-12-04 13:04:37 -0600

    imports RevenueContainer and adds it to the return of app

--- frontend/rev-tracker/src/App.js
+++ frontend/rev-tracker/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Login from './Login'
 import MainNavBar from './Nav'
 import MainFooter from './Footer';
+import RevenueContainer from './RevenueContainer';
 
 function App() {
   return (
     <div className="App">
       <MainNavBar />
-      
+      <RevenueContainer />
       <MainFooter />
     </div>
   );
